e64e8a5f4b0e202bd3c9aebc511757c39678371d
b046814 <ub046814@gmail.com>
2020-08-29 09:23:04 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password,    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :last_name,   format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_k, format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_k,  format: { with: /\A[ァ-ン]+\z/ }
+  validates :nickname, :last_name, :first_name, :last_name_k, :first_name_k, :birth_date, presence: true
+
 end
