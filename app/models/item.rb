c7e793ef6b9d0b14de0ff70846e3d32110e336b4
b046814 <ub046814@gmail.com>
2020-08-30 11:30:21 +0900

    テーブル作成と各モデル内記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :ship_expense
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :period
+
+  validates :image, :name, :description, :category_id, :condition_id, :ship_expense_id, :prefecture_id, :period_id, :price, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :ship_expense_id
+    validates :prefecture_id
+    validates :period_id
+  end
+
+  balongs_to :user
 end
