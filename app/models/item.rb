ae15f421896ee223c8cf7ad569b8a5edaacadd85
b046814 <ub046814@gmail.com>
2020-08-30 15:18:06 +0900

    itemモデルへバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,9 @@ class Item < ApplicationRecord
 
   validates :image, :name, :description, :category_id, :condition_id, :ship_expense_id, :prefecture_id, :period_id, :price, presence: true
 
+  validates :price, format: { with: /\A[\d]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
