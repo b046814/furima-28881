bf33c03f4021fe15a3191a2d7bed63de41db9bd2
b046814 <ub046814@gmail.com>
2020-09-04 12:34:20 +0900

    商品購入ページ表示機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :move_to_root, only: :index
+
+  def index
+  end
+
+  private
+  def move_to_root
+    @item = Item.find(params[:item_id])
+    return if user_signed_in? && @item.order == nil && current_user.id != @item.user_id
+    
+    redirect_to root_path
+  end
 end
