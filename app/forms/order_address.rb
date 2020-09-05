e43ba348be91dec11a04c3233651a6169b666336
b046814 <ub046814@gmail.com>
2020-09-05 22:40:16 +0900

    rubocopの指摘修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
-
   include ActiveModel::Model
-  attr_accessor :order_address, :user_id, :item_id, :token, :price, :postal_code, :prefecture_id, :city_name, :block_number, :building, :phone_number
+  attr_accessor :order_address, :user_id, :item_id, :token, :price, \
+                :postal_code, :prefecture_id, :city_name, :block_number, :building, :phone_number
 
   validates :token, :postal_code, :prefecture_id, :city_name, :block_number, :phone_number, presence: true
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -10,7 +10,7 @@ class OrderAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city_name: city_name, block_number: block_number, building: building, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city_name: city_name, \
+                   block_number: block_number, building: building, phone_number: phone_number, order_id: order.id)
   end
-
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -33,31 +33,31 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号にハイフンが無ければ保存できない' do
         @order_address.postal_code = '1235555'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
 
       it '郵便番号のハイフンの前が半角数字3文字で無ければ保存できない' do
         @order_address.postal_code = '12-3555'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
 
       it '郵便番号のハイフンの後ろが半角数字4文字で無ければ保存できない' do
         @order_address.postal_code = '123-355'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
 
       it '都道府県の情報が空では保存できない' do
         @order_address.prefecture_id = nil
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank", 'Prefecture is not a number')
       end
 
       it '都道府県情報のidが1の場合は保存できない' do
         @order_address.prefecture_id = 1
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@order_address.errors.full_messages).to include('Prefecture must be other than 1')
       end
 
       it '市区町村の情報が空では保存できない' do
@@ -81,15 +81,14 @@ RSpec.describe OrderAddress, type: :model do
       it '電話番号が半角数字12文字以上では保存できない' do
         @order_address.phone_number = '111222333444'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it '電話番号が半角数字以外では保存できない' do
-         @order_address.phone_number = '６６６７８９０'
-         @order_address.valid?
-         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        @order_address.phone_number = '６６６７８９０'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
-
     end
   end
 end
